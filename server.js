7af4aa930aba93e7090722f7bbffaaa82963b5f9
ringlander-apps <dev.ringlander@gmail.com>
2018-11-17 23:22:42 +0100

    Setup and USE bodyparser

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongooose = require("mongoose");
+const bodyParser = require("body-parser");
 
 //Pickup the route files
 
@@ -8,6 +9,10 @@ const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
 
 const app = express();
+
+//Setup and USE body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -26,10 +31,8 @@ app.use("/api/auth", auth);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
+//Setup and assign port variable
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
+//Server waiting for requests.....
 app.listen(port, () => console.log(`Server running on port: ${port}`));
