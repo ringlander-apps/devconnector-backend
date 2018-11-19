cbc3e9ac49766fb683c0e2fd7ccbcdd130a7de00
ringlander-apps <dev.ringlander@gmail.com>
2018-11-19 11:18:24 +0100

    Adjusting isEmpty method. Should be refactored...

--- validation/is-empty.js
+++ validation/is-empty.js
@@ -1,8 +1,21 @@
 const isEmpty = value => {
-  value === undefined ||
+  //console.log("Checking the input value: " + value);
+  if (value === undefined) {
+    return true;
+  }
+  if (value === null) {
+    return true;
+  }
+  if (
+    (typeof value === "object" && Object.keys(value).length === 0) ||
+    (typeof value === "string" && value.trim().length === 0)
+  ) {
+    return true;
+  }
+  /*value === undefined ||
     value === null ||
     (typeof value === "object" && Object.keys(value).length === 0) ||
-    (typeof value === "string" && value.trim().length === 0);
+    (typeof value === "string" && value.trim().length === 0);*/
 };
 
 module.exports = isEmpty;
