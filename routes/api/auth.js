bc6038d60d635698a08197859e163dc61d87dd9f
ringlander-apps <dev.ringlander@gmail.com>
2018-11-19 02:14:40 +0100

    Initializing server side validation procedures

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,6 +6,8 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//Load Input validation
+const validateRegisterInput = require("../../validation/register");
 //Load User model
 const User = require("../../models/User");
 
@@ -20,6 +22,11 @@ router.get("/test", (req, res) => {
 // @desc    Register new user
 // @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   //Use our User model to see if user matching provided email exists
   User.findOne({
     email: req.body.email
