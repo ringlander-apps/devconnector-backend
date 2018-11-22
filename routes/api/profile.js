3fa6d5dce862f69bfb974496f8103632072ed074
ringlander-apps <dev.ringlander@gmail.com>
2018-11-23 00:27:41 +0100

    Added validation by referencting the profile validation file Fixed typos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+//Load Input validation for Profile
+const validateProfileInput = require("../../validation/profile");
 //Load profile
 const Profile = require("../../models/Profile");
 //Load user
@@ -25,6 +27,7 @@ router.get(
   (req, res) => {
     const errors = {};
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
@@ -43,30 +46,34 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
     //Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
 
-    if (req.body.handle) profileFields.handle == req.body.handle;
-    if (req.body.company) profileFields.company == req.body.company;
-    if (req.body.website) profileFields.website == req.body.website;
-    if (req.body.location) profileFields.location == req.body.location;
-    if (req.body.bio) profileFields.bio == req.body.bio;
-    if (req.body.status) profileFields.status == req.body.status;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
     if (req.body.githubusername)
-      profileFields.githubusername == req.body.githubusername;
+      profileFields.githubusername = req.body.githubusername;
     //Skills
     if (typeof (req.body.skills !== "undefined")) {
       profileFields.skills = req.body.skills.split(",");
     }
     //Socials
     profileFields.social = {};
-    if (req.body.youtube) profileFields.social.youtube == req.body.youtube;
-    if (req.body.twitter) profileFields.social.twitter == req.body.twitter;
-    if (req.body.linkedin) profileFields.social.linkedin == req.body.linkedin;
-    if (req.body.facebook) profileFields.social.facebook == req.body.facebook;
-    if (req.body.instagram)
-      profileFields.social.yinstagram == req.bodyinstagrame;
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
     Profile.findOne({ user: req.user.id })
       .then(profile => {
