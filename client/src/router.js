8e22d9d9ff3e7974bb237b9343dc10500471b4fa
ringlander-apps <dev.ringlander@gmail.com>
2018-11-27 07:31:45 +0100

    Clean-up, removed references to boilerplate components/views

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
 import Landing from "@/components/layout/Landing.vue";
 import Login from "@/components/auth/Login.vue";
 import Register from "@/components/auth/Register.vue";
@@ -25,16 +24,16 @@ export default new Router({
       path: "/",
       name: "landing",
       component: Landing
-    },
-
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
+
+    // {
+    //   path: "/about",
+    //   name: "about",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ "./views/About.vue")
+    // }
   ]
 });
