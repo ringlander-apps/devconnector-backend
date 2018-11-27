a89edf3347e3ef6beb4dabbad3cbf1882da6b010
ringlander-apps <dev.ringlander@gmail.com>
2018-11-27 23:38:58 +0100

    Added limited state management to handle Register and validation errors.

--- client/src/components/auth/Register.vue
+++ client/src/components/auth/Register.vue
@@ -13,19 +13,23 @@
               <input
                 v-model="state.name"
                 type="text"
+                v-bind:class="{'is-invalid':isInvalidName}"
                 class="form-control form-control-lg"
                 placeholder="Name"
                 name="name"
               >
+              <div v-if="isInvalidName" class="invalid-feedback">{{this.state.errors.name}}</div>
             </div>
             <div class="form-group">
               <input
                 v-model="state.email"
                 type="email"
+                v-bind:class="{'is-invalid':isInvalidEmail}"
                 class="form-control form-control-lg"
                 placeholder="Email Address"
                 name="email"
               >
+              <div v-if="isInvalidEmail" class="invalid-feedback">{{this.state.errors.email}}</div>
               <small
                 class="form-text text-muted"
               >This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
@@ -34,19 +38,23 @@
               <input
                 v-model="state.password"
                 type="password"
+                v-bind:class="{'is-invalid':isInvalidPassword}"
                 class="form-control form-control-lg"
                 placeholder="Password"
                 name="password"
               >
+              <div v-if="isInvalidPassword" class="invalid-feedback">{{this.state.errors.password}}</div>
             </div>
             <div class="form-group">
               <input
                 v-model="state.password2"
                 type="password"
+                v-bind:class="{'is-invalid':isInvalidPassword2}"
                 class="form-control form-control-lg"
                 placeholder="Confirm Password"
                 name="password2"
               >
+              <div class="invalid-feedback">{{this.state.errors.password2}}</div>
             </div>
             <input type="submit" class="btn btn-info btn-block mt-4">
           </form>
@@ -72,9 +80,34 @@ export default {
       }
     };
   },
+  computed: {
+    isInvalidName() {
+      if (this.state.errors) {
+        return this.state.errors.name !== undefined ? true : false;
+      }
+    },
+    isInvalidEmail() {
+      if (this.state.errors) {
+        return this.state.errors.email !== undefined ? true : false;
+      }
+    },
+    isInvalidPassword() {
+      if (this.state.errors) {
+        return this.state.errors.password !== undefined ? true : false;
+      }
+    },
+    isInvalidPassword2() {
+      if (this.state.errors) {
+        return this.state.errors.password2 !== undefined ? true : false;
+      }
+    }
+  },
   methods: {
+    setErrors(errors) {
+      this.state.errors = errors;
+    },
     processForm(e) {
-      const { name, email, password, password2 } = this.state;
+      let { name, email, password, password2 } = this.state;
 
       const newUser = {
         name,
@@ -82,12 +115,13 @@ export default {
         password,
         password2
       };
-
       Axios.post("api/auth/register", newUser)
         .then(res => {
           console.log(res.data);
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+          this.setErrors(err.response.data);
+        });
     }
   }
 };
