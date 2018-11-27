46f230154ec513f1c8260bd12a6b2f04cdf5822d
ringlander-apps <dev.ringlander@gmail.com>
2018-11-27 23:38:03 +0100

    Removed unused code in router

--- client/src/router.js
+++ client/src/router.js
@@ -25,15 +25,5 @@ export default new Router({
       name: "landing",
       component: Landing
     }
-
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: "about" */ "./views/About.vue")
-    // }
   ]
 });
