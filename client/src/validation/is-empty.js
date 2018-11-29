a943f87f6d7be2ba9493cddcdfae258094703580
ringlander-apps <dev.ringlander@gmail.com>
2018-11-30 00:59:35 +0100

    Function for checking objects are empty

---
+++ client/src/validation/is-empty.js
@@ -0,0 +1,21 @@
+const isEmpty = value => {
+  //console.log("Checking the input value: " + value);
+  if (value === undefined) {
+    return true;
+  }
+  if (value === null) {
+    return true;
+  }
+  if (
+    (typeof value === "object" && Object.keys(value).length === 0) ||
+    (typeof value === "string" && value.trim().length === 0)
+  ) {
+    return true;
+  }
+  /*value === undefined ||
+    value === null ||
+    (typeof value === "object" && Object.keys(value).length === 0) ||
+    (typeof value === "string" && value.trim().length === 0);*/
+};
+
+export default isEmpty;
