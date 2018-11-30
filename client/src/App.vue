c01cf5e7212faa9b2b3a8b41189c551065cce995
ringlander-apps <dev.ringlander@gmail.com>
2018-11-30 01:21:24 +0100

    Clean-up

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,18 @@
 <template>
   <div id="app">
-    <Navbar />
+    <Navbar/>
     <router-view/>
     <Footer/>
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-     -->
   </div>
 </template>
 <script>
 import Navbar from "@/components/layout/Navbar.vue";
 import Footer from "@/components/layout/Footer.vue";
-//import Landing from "@/components/layout/Landing.vue";
 
 export default {
   components: {
     Navbar,
     Footer
-    //Landing
   }
 };
 </script>
