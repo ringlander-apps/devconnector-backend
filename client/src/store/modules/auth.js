3c1fbd90b9f54de030eb60a0d3be728cd1bb1597
ringlander-apps <dev.ringlander@gmail.com>
2018-11-30 01:16:16 +0100

    Lot of changes done to handle login - isAuthenticated is iniitated by checking if the decoded token infomation is empty or not from localStorage
    -user is initiated by trying to decode token from localStorage
    
    -loginErrors object added to the state
    
    GETTERS;
    - LOGIN_ERROR_EMAIL
    returns the email error object from state.loginErrors
    - LOGIN_ERROR_PW
    returns the password error obejct from state.loginErrors
    
    MUTATIONS:
    SET_CURRENT_USER:
    - sets isAuthenticated using isEmpty method on provided payload
    - sets user from provided payload
    
    SET_LGOIN_ERRORS:
    - sets the authErrors state with provided errors object
    RESET_LOGIN_ERRORS:
    -setst the authErrors to an empty object upon successful login
    
    SET_TOKEN_DATA:
    - writes token infomation to localStorage
    --------------------------------------------------------------
    --------------------------------------------------------------
    ACTIONS:
    --------------
    LOGIN_USER_REQUEST:
    
    Calls loginUser on APIService object providing userData
    passed in to the action.
    When promise resolves following happens:
    - Token is set on the axios client using setAuthToken
    - Token information is set using mutation SET_TOKEN_DATA
    - USER is decoded from the token and set using SET_CURRENT_USER
    - RESET_LOGIN_ERRORS mutation are commited
    --------------------------------------------------------------------------
    LOGOUT_USER_REQUEST
    
    Clears out the token information in the header object on the Axios api client.
    Clears out token data by commiting SET_TOKEN_DATA with null value object
    Clears out current user by commiting SET_CURRENT_USER with null value object.

--- client/src/store/modules/auth.js
+++ client/src/store/modules/auth.js
@@ -5,11 +5,17 @@ import {
 } from "../actions/auth";
 
 import APIService from "@/services/APIService";
+import jwt_decode from "jwt-decode";
+import isEmpty from "@/validation/is-empty";
 
 const state = {
-  isAuthenticated: false,
-  user: {},
-  authErrors: {}
+  isAuthenticated: !isEmpty(jwt_decode(localStorage.getItem("devcon-token"))),
+  user: jwt_decode(localStorage.getItem("devcon-token")),
+  accessToken: localStorage.getItem("devcon-token"),
+  expiresAt: localStorage.getItem("devcon-token-expires_at"),
+
+  authErrors: {},
+  loginErrors: {}
 };
 const getters = {
   IS_AUTHENTICATED: state => {
@@ -33,28 +39,54 @@ const getters = {
   AUTH_ERROR_PW2: state => {
     const { password2 } = state.authErrors;
     return password2 !== "" ? password2 : "";
+  },
+  LOGIN_ERROR_EMAIL: state => {
+    const { email } = state.loginErrors;
+    return email !== "" ? email : "";
+  },
+  LOGIN_ERROR_PW: state => {
+    const { password } = state.loginErrors;
+    return password !== "" ? password : "";
   }
 };
 const mutations = {
-  SET_AUTHENTICATED: (state, payload) => {
-    state.isAuthenticated = payload;
-  },
-  SET_CURRENT_USER: (state, user) => {
-    state.user = user;
+  SET_CURRENT_USER: (state, payload) => {
+    state.isAuthenticated = !isEmpty(payload);
+    state.user = payload;
   },
   SET_AUTH_ERRORS: (state, errors) => {
     state.authErrors = errors;
   },
   RESET_AUTH_ERRORS: state => {
     state.authErrors = {};
+  },
+  SET_LOGIN_ERRORS: (state, errors) => {
+    state.loginErrors = errors.errors;
+  },
+  RESET_LOGIN_ERRORS: state => {
+    state.loginErrors = {};
+  },
+  SET_TOKEN_DATA: (state, authData) => {
+    if (authData) {
+      localStorage.setItem("devcon-token", JSON.stringify(authData.token));
+      localStorage.setItem(
+        "devcon-token-expires_at",
+        JSON.stringify(authData.expires_at)
+      );
+    } else {
+      localStorage.removeItem("devcon-token");
+      localStorage.removeItem("devcon-token-expires_at");
+    }
   }
 };
 const actions = {
+  /**
+   * ACTION which registers a new user
+   */
   [REGISTER_USER_REQUEST]: ({ commit }, userData) => {
     return new Promise((resolve, reject) => {
       APIService.registerUser(userData)
         .then(result => {
-          commit("SET_CURRENT_USER", result);
           commit("RESET_AUTH_ERRORS");
           resolve({ status: 200, success: true });
         })
@@ -63,8 +95,49 @@ const actions = {
           reject({ status: 400, success: false });
         });
     });
+  },
+  /**
+   * ACTION logging in the user by calling the axios api client wrapper APIService
+   * Upon successful login, access_token is stored in local storage, user data is decoded from the token
+   * User is authenticated
+   */
+  [LOGIN_USER_REQUEST]: ({ commit }, userData) => {
+    return new Promise((resolve, reject) => {
+      APIService.loginUser(userData)
+        .then(result => {
+          //Get the token
+          const { token } = result.data;
+          //SetAuthToken on APIService calls
+          APIService.setAuthToken(token);
+          //Commit mutation
+          commit("SET_TOKEN_DATA", result.data);
+          //Decode user information from the token
+          commit("SET_CURRENT_USER", jwt_decode(token));
+          commit("RESET_LOGIN_ERRORS");
+          resolve({ status: 200, success: true });
+        })
+        .catch(err => {
+          console.log(err.response);
+          commit("SET_LOGIN_ERRORS", err.response.data);
+          reject({ status: 400, success: false });
+        });
+    });
+  },
+  /**
+   * ACTTION which should log out the current user
+   * That means:
+   * Remove token information from the default.headers in the axios api client ()
+   * Clear out token data from localStorage
+   * Set current user to null
+   */
+  [LOGOUT_USER_REQUEST]: ({ commit }) => {
+    console.log("Here");
+    APIService.setAuthToken("");
+    commit("SET_TOKEN_DATA", null);
+    commit("SET_CURRENT_USER", null);
   }
 };
+
 export default {
   state,
   getters,
