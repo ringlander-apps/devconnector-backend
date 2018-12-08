74535ff347825590f53b5681a04e247679eec34e
ringlander-apps <dev.ringlander@gmail.com>
2018-12-08 11:39:38 +0100

    Added development key to cloud atlas cluster

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
   mongoURI:
     "mongodb://dc-user:R1ngland3r@ds135917.mlab.com:35917/dev-connector",
+  atlasURI:
+    "mongodb://devConUser:devConUser@clustereurope-shard-00-00-1xqvp.azure.mongodb.net:27017,clustereurope-shard-00-01-1xqvp.azure.mongodb.net:27017,clustereurope-shard-00-02-1xqvp.azure.mongodb.net:27017/devConnector?ssl=true&replicaSet=ClusterEurope-shard-0&authSource=admin&retryWrites=true",
   secretOrKey: "mysecret"
 };
