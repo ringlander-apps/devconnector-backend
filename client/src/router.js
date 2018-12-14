cf215272f8bd492304a97432dafdabd5ff480881
ringlander-apps <dev.ringlander@gmail.com>
2018-12-14 09:39:37 +0100

    Added functionality for new paths Added per-route guard functionality using Auth

--- client/src/router.js
+++ client/src/router.js
@@ -4,12 +4,18 @@ import Landing from "@/components/layout/Landing.vue";
 import Login from "@/components/auth/Login.vue";
 import Register from "@/components/auth/Register.vue";
 import Dashboard from "@/views/Dashboard.vue";
+import CreateProfile from "@/components/profile/add/CreateProfile.vue";
+import Profile from "@/views/Profile";
+
+import store from "./store/index";
+import jwt_decode from "jwt-decode";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
+
   routes: [
     {
       path: "/login",
@@ -29,7 +35,43 @@ export default new Router({
     {
       path: "/dashboard",
       name: "dashboard",
-      component: Dashboard
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/create-profile",
+      name: "create-profile",
+      component: CreateProfile,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/profile/:handle",
+      name: "profile",
+      component: Profile,
+      // meta: {
+      //   requiresAuth: true
+      // },
+      props: true
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    const { IS_AUTHENTICATED } = store.getters;
+
+    if (IS_AUTHENTICATED) {
+      next();
+    } else {
+      next({ name: "login" });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
