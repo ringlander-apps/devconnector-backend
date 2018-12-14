36ab27a22790db14d3215f8c2cb16f9dd9ec5b4f
ringlander-apps <dev.ringlander@gmail.com>
2018-12-14 09:31:39 +0100

    Created Spinner functional component which encapsulates spinner.gif

---
+++ client/src/components/common/Spinner.vue
@@ -0,0 +1,21 @@
+<script>
+import spinner from "./spinner.gif";
+export default {
+  functional: true,
+  props: {},
+  render(h, { data, props, children }) {
+    return (
+      <div>
+        <img
+          src={spinner}
+          alt="Loading..."
+          style={{ width: "200px", margin: "auto", display: "block" }}
+        />
+      </div>
+    );
+  }
+};
+</script>
+
+<style scoped>
+</style>
