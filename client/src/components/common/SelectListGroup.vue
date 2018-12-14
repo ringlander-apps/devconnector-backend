1794f18a9dd8ce15ce54f4de4a111bd3d26289a6
ringlander-apps <dev.ringlander@gmail.com>
2018-12-14 09:30:24 +0100

    Created new functional component SelectListGroup

---
+++ client/src/components/common/SelectListGroup.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  functional: true,
+  props: {
+    options: {
+      type: Array
+    },
+    value: {
+      type: String
+    },
+    info: {
+      type: String
+    },
+    error: {
+      type: String
+    },
+    disabled: {
+      type: Boolean
+    }
+  },
+  onSelected(event) {},
+  render(h, { data, props, children }) {
+    const onSelected = data.on["change"];
+
+    let selectOptions = props.options.map(option => {
+      return (
+        <option key={option.label} value={option.value}>
+          {option.label}
+        </option>
+      );
+    });
+    let wrapperElement = (
+      <div class="form-group">
+        <select
+          name={data.attrs.name}
+          class={data.class}
+          onChange={e => onSelected(e, e.target.value)}
+        >
+          {selectOptions}
+        </select>
+        {props.error && <div class="invalid-feedback">{props.error}</div>}
+        {props.info && <small class="form-text text-muted">{props.info}</small>}
+      </div>
+    );
+    return wrapperElement;
+  }
+};
+</script>
