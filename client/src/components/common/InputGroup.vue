a98d9517d65b84dad793000f986326aead40acd8
ringlander-apps <dev.ringlander@gmail.com>
2018-12-14 09:27:34 +0100

    Created new functional component InputGroup Input group wraps an icon together with an html input element

---
+++ client/src/components/common/InputGroup.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  functional: true,
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    info: {
+      type: String
+    },
+    error: {
+      type: String
+    },
+    icon: {
+      type: String
+    },
+    disabled: {
+      type: Boolean
+    }
+  },
+  render(h, { data, props, children }) {
+    const onChange = data.on["input"];
+    let element = (
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text">
+            <i class={props.icon} />
+          </span>
+        </div>
+        <input
+          name={data.attrs.name}
+          class={data.class}
+          placeholder={data.attrs.placeholder}
+          type={data.attrs.type}
+          value={props.value}
+          onChange={e => onChange(e, e.target.value)}
+        />
+        {props.error && <div class="invalid-feedback">{props.error}</div>}
+      </div>
+    );
+    return element;
+  }
+};
+</script>
