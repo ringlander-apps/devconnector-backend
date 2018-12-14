3af425a22913924175af55db7b7bb14ef5806b73
ringlander-apps <dev.ringlander@gmail.com>
2018-12-14 09:34:41 +0100

    Created new functional component which wraps text-area info component

---
+++ client/src/components/common/TextAreaFieldGroup.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  functional: true,
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    info: {
+      type: String
+    },
+    error: {
+      type: String
+    },
+    disabled: {
+      type: Boolean
+    }
+  },
+  render(h, { data, props, children }) {
+    const onChange = data.on["change"];
+    let wrapperElement = (
+      <div class="form-group">
+        <textarea
+          name={data.attrs.name}
+          class={data.class}
+          placeholder={data.attrs.placeholder}
+          onChange={e => onChange(e, e.target.value)}
+        />
+        <small class="form-text text-muted">{props.info}</small>
+      </div>
+    );
+    return wrapperElement;
+  }
+};
+</script>
