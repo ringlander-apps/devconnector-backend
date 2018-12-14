7c63c0e4df77b0f28bfeb18ac3d2fb28ff1d2c44
ringlander-apps <dev.ringlander@gmail.com>
2018-12-14 09:35:15 +0100

    Created ProfileActions component

--- client/src/components/profile/ProfileActions.vue
+++ client/src/components/profile/ProfileActions.vue
@@ -1,9 +1,28 @@
-<template>
-  <div></div>
-</template>
-
 <script>
-export default {};
+export default {
+  functional: true,
+  render(h, { data, props, children }) {
+    let element = (
+      <div>
+        <div class="btn-group mb-4" role="group">
+          <router-link to="/landing" class="btn btn-light">
+            <i class="fas fa-user-circle text-info mr-1" />
+            Edit Profile
+          </router-link>
+          <a href="add-experience.html" class="btn btn-light">
+            <i class="fab fa-black-tie text-info mr-1" />
+            Add Experience
+          </a>
+          <a href="add-education.html" class="btn btn-light">
+            <i class="fas fa-graduation-cap text-info mr-1" />
+            Add Education
+          </a>
+        </div>
+      </div>
+    );
+    return element;
+  }
+};
 </script>
 
 <style scoped>
