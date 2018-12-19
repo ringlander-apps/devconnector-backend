bce623452a2c8fe18ea308421e6a665fbdc74053
ringlander-apps <dev.ringlander@gmail.com>
2018-12-19 12:38:08 +0100

    Created new CheckBoxInputGroup component which is has a div wrapper surrounding an: -- input element of type 'checkbox' -- label element

---
+++ client/src/components/common/CheckBoxInputGroup.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  functional: true,
+  props: {
+    value: {
+      type: String
+    },
+    label: {
+      type: String
+    },
+    disabled: {
+      type: Boolean
+    }
+  },
+  render(h, { data, props, children }) {
+    const onChange = data.on["change"];
+    let element = (
+      <div class="form-check mb-4">
+        <input
+          class={data.class}
+          type="checkbox"
+          name={data.attrs.name}
+          onChange={e => onChange(e.target, e.target.checked)}
+          checked={data.attrs.checked}
+          disabled={props.disabled}
+        />
+        <label class="form-check-label" HtmlFor={data.attrs.name}>
+          {props.label}
+        </label>
+      </div>
+    );
+
+    return element;
+  }
+};
+</script>
+
