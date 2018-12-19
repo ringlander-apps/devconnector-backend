b45d9b6f31de94b63447202645f0434b988fae6e
ringlander-apps <dev.ringlander@gmail.com>
2018-12-19 18:12:59 +0100

    Created DateFieldGroup component div element which wraps an  --- h6 --- input of type date
    also a div with error message will be rendered upon error

---
+++ client/src/components/common/DateFieldGroup.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  functional: true,
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String
+    },
+    error: {
+      type: String
+    },
+    disabled: {
+      type: Boolean
+    }
+  },
+  render(h, { data, props, children }) {
+    const onChange = data.on["input"];
+    let element = (
+      <div>
+        <h6>{props.label}</h6>
+        <div class="form-group">
+          <input
+            type="date"
+            class={data.class}
+            name={data.attrs.name}
+            value={props.value}
+            placeholder={data.attrs.placeholder}
+            disabled={props.disabled}
+            onChange={e => onChange(e.target, e.target.value)}
+          />
+          {props.error && <div class="invalid-feedback">{props.error}</div>}
+        </div>
+      </div>
+    );
+
+    return element;
+  }
+};
+</script>
+
