a33866e18087949ce1697170f12c4b144d0cb19e
ringlander-apps <dev.ringlander@gmail.com>
2019-01-07 09:17:59 +0100

    Component for displaying the feed of posts
    Uses component:
    PostCard for displaing content in each post.

---
+++ client/src/components/post/PostFeed.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="posts">
+    <div v-if="posts.length>0">
+      <PostCard
+        v-for="post in this.posts"
+        :showActions="true"
+        :key="post._id"
+        :post="post"
+        :title="post.text"
+      />
+    </div>
+    <div v-else>No posts found</div>
+  </div>
+</template>
+
+<script>
+import PostCard from "@/components/post/PostCard";
+export default {
+  components: {
+    PostCard
+  },
+  name: "post-feed",
+  props: {
+    title: {
+      type: String
+    },
+    posts: {
+      type: Array
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
