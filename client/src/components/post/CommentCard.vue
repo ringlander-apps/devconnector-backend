6f36ce731ddee6589d654bb3315648140dc930bf
ringlander-apps <dev.ringlander@gmail.com>
2019-01-07 10:44:04 +0100

    Added delete functionality If logged in user is the same as the comment user, then delete should be enabled.
    Added method handleDeleteComme
    An DeleteObject is created containing
    postID,
    commentID,
    calls mapped action DELETE_COMMENT_REQUEST providing
    the deleteobject

--- client/src/components/post/CommentCard.vue
+++ client/src/components/post/CommentCard.vue
@@ -10,20 +10,55 @@
       </div>
       <div class="col-md-10">
         <p class="lead">{{this.comment.text}}</p>
+        <div>
+          <button
+            v-if="USER.name===comment.name"
+            @click.prevent="handleDeleteComment(comment._id)"
+            type="button"
+            class="btn btn-danger mr-1"
+          >
+            <i class="fas fa-times"></i>
+            Delete Comment
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+
 export default {
   name: "comment-card",
   props: {
     title: {
       type: String
     },
+    postID: {
+      type: String
+    },
     comment: {
       type: Object
+    }
+  },
+  computed: {
+    ...mapGetters(["USER"])
+  },
+  methods: {
+    ...mapActions(["DELETE_COMMENT_REQUEST"]),
+    handleDeleteComment(commentId) {
+      const deleteObject = {
+        commentID: commentId,
+        postID: this.postID
+      };
+      this.DELETE_COMMENT_REQUEST(deleteObject)
+        .then(result => {
+          console.log(result);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
 };
