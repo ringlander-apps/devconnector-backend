f75074b22e81cd932e44436236dff68082d21237
ringlander-apps <dev.ringlander@gmail.com>
2019-01-07 09:02:30 +0100

    View used for illegal routes.

---
+++ client/src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1 class="display-4">Page Not Found</h1>
+    <p>Sorry, this page does not exist</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "not-found"
+};
+</script>
+
+<style scoped>
+</style>
